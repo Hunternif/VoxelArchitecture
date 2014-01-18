e202e87bb2ba5ea3cff1c87bcbe8f3a37b4b87e5
Hunternif <hunternif@gmail.com>
2014-01-18 16:56:27 +0400

    Setup basic mod files for VoxArchMCMod.

---
+++ VoxArchMCMod/src/hunternif/voxarch/mc/VoxArchMod.java
@@ -0,0 +1,28 @@
+package hunternif.voxarch.mc;
+
+import java.util.logging.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid=VoxArchMod.ID, name=VoxArchMod.NAME, version=VoxArchMod.VERSION)
+@NetworkMod(clientSideRequired=false, serverSideRequired=true)
+public class VoxArchMod {
+	public static final String ID = "voxarch";
+	public static final String NAME = "Voxel Architecture";
+	public static final String VERSION = "@@MOD_VERSION@@";
+	public static final String CHANNEL = ID;
+	
+	@Instance(ID)
+	public static VoxArchMod instance;
+	
+	public static Logger logger;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
+	}
+}
