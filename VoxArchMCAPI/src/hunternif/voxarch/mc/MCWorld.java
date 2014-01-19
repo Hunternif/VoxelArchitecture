b0d9907074e9952b2611499b7cd4097a5a60e065
Hunternif <hunternif@gmail.com>
2014-01-19 18:37:37 +0400

    ExtBlockDataMC is saved in MCWorld and can be retrieved.

--- VoxArchMCAPI/src/hunternif/voxarch/mc/MCWorld.java
+++ VoxArchMCAPI/src/hunternif/voxarch/mc/MCWorld.java
@@ -2,6 +2,11 @@ package hunternif.voxarch.mc;
 
 import hunternif.voxarch.storage.BlockData;
 import hunternif.voxarch.storage.IBlockStorage;
+import hunternif.voxarch.util.IntVec3;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.world.World;
 
 /** Adapter between Minecraft World and IBlockStorage. */
@@ -11,6 +16,10 @@ public class MCWorld implements IBlockStorage {
 	 * at the very minimum. */
 	private final BlockData reusableData = new BlockData(0);
 	
+	/** Map in which ExtBlockDataMC is stored, because it can't be retrieved
+	 * from the world via block id and metadata. */
+	private final Map<IntVec3, ExtBlockDataMC> extBlocks = new HashMap<IntVec3, ExtBlockDataMC>();
+	
 	private final World world;
 	
 	public MCWorld(World world) {
@@ -19,6 +28,8 @@ public class MCWorld implements IBlockStorage {
 	
 	@Override
 	public BlockData getBlock(int x, int y, int z) {
+		ExtBlockDataMC extBlock = extBlocks.get(new IntVec3(x, y, z));
+		if (extBlock != null) return extBlock;
 		reusableData.setId(world.getBlockId(x, y, z));
 		if (reusableData.getId() == 0) return null;
 		reusableData.setMetadata(world.getBlockMetadata(x, y, z));
@@ -29,7 +40,8 @@ public class MCWorld implements IBlockStorage {
 	public void setBlock(int x, int y, int z, BlockData block) {
 		// Flag 2 will send the change to clients
 		if (block instanceof ExtBlockDataMC) {
-			((ExtBlockDataMC)block).onPasteIntoWorld(world, x, y, z);
+			((ExtBlockDataMC) block).onPasteIntoWorld(world, x, y, z);
+			extBlocks.put(new IntVec3(x, y, z), (ExtBlockDataMC) block);
 		} else {
 			world.setBlock(x, y, z, block.getId(), block.getMetadata(), 2);
 		}
