88f9bd672af2d4a18ff922d0550ce79435f26a47
Hunternif <hunternif@gmail.com>
2014-01-21 16:44:58 +0400

    Using Forge RotationHelper for Minecraft-specific block rotations. Dropped submodule StructureGenAPI

--- VoxArchMCAPI/src/hunternif/voxarch/mc/ExtBlockDataMC.java
+++ VoxArchMCAPI/src/hunternif/voxarch/mc/ExtBlockDataMC.java
@@ -6,6 +6,10 @@ import net.minecraft.world.World;
 /**
  * Extended Minecraft-specific block data. Use this class to paste spawn things
  * like paintings, item frames, mob spawners etc.
+ * <p>
+ * This block data type is not persistent, meaning that it can only be retrieved
+ * back from the world if it was previously set in without restarting the JVM.
+ * </p>
  * @author Hunternif
  */
 public abstract class ExtBlockDataMC extends BlockData {
--- VoxArchMCAPI/src/hunternif/voxarch/mc/MCWorld.java
+++ VoxArchMCAPI/src/hunternif/voxarch/mc/MCWorld.java
@@ -2,19 +2,38 @@ package hunternif.voxarch.mc;
 
 import hunternif.voxarch.storage.BlockData;
 import hunternif.voxarch.storage.IBlockStorage;
+import hunternif.voxarch.util.BlockOrientation;
 import hunternif.voxarch.util.IntVec3;
 
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.RotationHelper;
 
 /**
- * Adapter between Minecraft World and IBlockStorage.
+ * Adapter between Minecraft World and IBlockStorage. Note that BlockOrientation
+ * is transformed into metadata in setBlock(), but the reverse doesn't happen in
+ * getBlock().
  * @author Hunternif
  */
 public class MCWorld implements IBlockStorage {
 
+	/** Bridges Forge block rotation enum with my custom one. Used to apply
+	 * correct metadata when pasting rotated blocks into the world. */
+	private static final EnumMap<BlockOrientation, ForgeDirection> forgeOrientMap
+			= new EnumMap<BlockOrientation, ForgeDirection>(BlockOrientation.class);
+	static {
+		forgeOrientMap.put(BlockOrientation.NONE, ForgeDirection.UNKNOWN);
+		forgeOrientMap.put(BlockOrientation.SOUTH, ForgeDirection.SOUTH);
+		forgeOrientMap.put(BlockOrientation.NORTH, ForgeDirection.NORTH);
+		forgeOrientMap.put(BlockOrientation.EAST, ForgeDirection.EAST);
+		forgeOrientMap.put(BlockOrientation.WEST, ForgeDirection.WEST);
+	}
+	
 	/** BlockData that is returned in every call, to keep the memory overhead
 	 * at the very minimum. */
 	private final BlockData reusableData = new BlockData(0);
@@ -47,6 +66,9 @@ public class MCWorld implements IBlockStorage {
 			extBlocks.put(new IntVec3(x, y, z), (ExtBlockDataMC) block);
 		} else {
 			world.setBlock(x, y, z, block.getId(), block.getMetadata(), 2);
+			// Apply rotation:
+			RotationHelper.rotateVanillaBlock(Block.blocksList[block.getId()],
+					world, x, y, z, forgeOrientMap.get(block.getOrientaion()));
 		}
 	}
 
