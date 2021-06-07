593d6eeec4550b19b365bd9cac2740a7e0d062cf
Hunternif <hunternif@gmail.com>
2021-06-07 22:57:03 +0100

    Snapshot test assert prints all failed tests

--- core/src/test/kotlin/hunternif/voxarch/snapshot/Snapshots.kt
+++ core/src/test/kotlin/hunternif/voxarch/snapshot/Snapshots.kt
@@ -26,7 +26,10 @@ class Snapshots {
     fun verifyAllSnapshots() {
         clearSnapshotsDir()
         generateAllSnapshots()
-        doVerifyAll()
+        val errors = doVerifyAll()
+        assert(errors.isEmpty()) {
+            "Snapshots failed:\n" + errors.joinToString("\n")
+        }
     }
 
     private fun clearSnapshotsDir() = FileUtils.deleteDirectory(SNAPSHOTS_DIR.toFile())
@@ -38,26 +41,36 @@ class Snapshots {
         }
     }
 
-    private fun doVerifyAll() {
+    /** Returns errors */
+    private fun doVerifyAll(): List<String> {
+        val errors = mutableListOf<String>()
         REFERENCES_DIR.onDirs { refDir ->
             refDir.onFiles { refFile ->
                 val testFile = SNAPSHOTS_DIR.resolve("${refDir.fileName}/${refFile.fileName}")
-                assert(Files.exists(testFile)) { "Missing snapshot $refFile" }
-                compareImages(refFile, testFile)
+                if (Files.exists(testFile)) {
+                    errors.addAll(compareImages(refFile, testFile))
+                } else {
+                    errors.add("Missing snapshot $refFile")
+                }
             }
         }
+        return errors
     }
 
-    private fun compareImages(ref: Path, test: Path) {
+    /** Returns errors */
+    private fun compareImages(ref: Path, test: Path): List<String> {
         val refImage = ImageIO.read(ref.toFile())
         val testImage = ImageIO.read(test.toFile())
         assert(refImage.width == testImage.width) { "width mismatch in $ref" }
         assert(refImage.height == testImage.height) { "height mismatch in $ref" }
         for (x in 0 until refImage.width) {
             for (y in 0 until refImage.height) {
-                assert(refImage.getRGB(x, y) == testImage.getRGB(x, y)) { "image mismatch in $ref" }
+                if (refImage.getRGB(x, y) != testImage.getRGB(x, y)) {
+                    return listOf("image mismatch in $ref")
+                }
             }
         }
+        return emptyList()
     }
 
     companion object {
