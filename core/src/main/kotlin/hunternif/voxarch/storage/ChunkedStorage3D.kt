3b9cd205af6363634bbfe8f250e0cc61472e1005
Hunternif <hunternif@gmail.com>
2022-01-08 18:20:26 +0000

    Implement ChunkedStorage3D

---
+++ core/src/main/kotlin/hunternif/voxarch/storage/ChunkedStorage3D.kt
@@ -0,0 +1,82 @@
+package hunternif.voxarch.storage
+
+import com.google.common.annotations.VisibleForTesting
+import hunternif.voxarch.vector.Array3D
+import hunternif.voxarch.vector.IntVec3
+import kotlin.math.floor
+
+class ChunkedStorage3D<T>(
+    private val chunkSize: IntVec3,
+    private val chunkFactory: () -> Array3D<T?>,
+) : IStorage3D<T?> {
+
+    companion object {
+        inline operator fun <reified T> invoke(
+            chunkWidth: Int = 16,
+            chunkHeight: Int = 16,
+            chunkLength: Int = 16,
+        ) = ChunkedStorage3D<T>(IntVec3(chunkWidth, chunkHeight, chunkLength)) {
+            Array3D(chunkWidth, chunkHeight, chunkLength, null)
+        }
+    }
+
+    override var minX: Int = 0
+        private set
+    override var maxX: Int = 0
+        private set
+    override var minY: Int = 0
+        private set
+    override var maxY: Int = 0
+        private set
+    override var minZ: Int = 0
+        private set
+    override var maxZ: Int = 0
+        private set
+
+    @PublishedApi internal var _size = 0
+    override val size: Int get() = _size
+
+    @VisibleForTesting
+    internal val chunkMap = mutableMapOf<IntVec3, Array3D<T?>>()
+    /** Reusable instance of a key into [chunkMap] */
+    private val chunkKey = IntVec3(0, 0, 0)
+    /** Reusable instance of coordinates inside a chunk */
+    private val arrayCoords = IntVec3(0, 0, 0)
+
+    val width: Int get() = if (size == 0) 0 else maxX - minX + 1
+    val height: Int get() = if (size == 0) 0 else maxY - minY + 1
+    val length: Int get() = if (size == 0) 0 else maxZ - minZ + 1
+
+    override fun get(x: Int, y: Int, z: Int): T? {
+        val key = getChunkKey(x, y, z)
+        return chunkMap[key]?.let { it[getArrayCoords(key, x, y, z)] }
+    }
+
+    override fun set(x: Int, y: Int, z: Int, v: T?) {
+        val key = getChunkKey(x, y, z)
+        val chunk = chunkMap[key]
+            ?: chunkFactory().also { chunkMap[key.clone()] = it }
+        val coords = getArrayCoords(key, x, y, z)
+        val prevVal = chunk[coords]
+        chunk[coords] = v
+        if (v == null && prevVal != null) _size--
+        if (v != null && prevVal == null) _size++
+    }
+
+    /** Maps voxel coordinates to the key of the chunk that contains them.
+     * Returns [chunkKey] */
+    private fun getChunkKey(x: Int, y: Int, z: Int): IntVec3 =
+        chunkKey.set(
+            floor(x.toFloat() / chunkSize.x).toInt(),
+            floor(y.toFloat() / chunkSize.y).toInt(),
+            floor(z.toFloat() / chunkSize.z).toInt(),
+        )
+
+    /** Maps voxel coordinates to inside a chunk. Returns [arrayCoords]. */
+    private fun getArrayCoords(chunkKey: IntVec3, x: Int, y: Int, z: Int): IntVec3 =
+        arrayCoords.set(
+            x - chunkKey.x * chunkSize.x,
+            y - chunkKey.y * chunkSize.y,
+            z - chunkKey.z * chunkSize.z,
+        )
+}
---
+++ core/src/test/kotlin/hunternif/voxarch/storage/ChunkedStorage3DTest.kt
@@ -0,0 +1,50 @@
+package hunternif.voxarch.storage
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ChunkedStorage3DTest {
+    @Test
+    fun `read and write to multiple chunks`() {
+        val a = ChunkedStorage3D<Char>(2, 3, 4)
+        assertEquals(null, a[0, 0, 0])
+        assertEquals(null, a[123, 456, -234])
+        assertEquals(0, a.chunkMap.size)
+
+        a[0, 0, 0] = 'a'
+        assertEquals(1, a.size)
+        assertEquals('a', a[0, 0, 0])
+        assertEquals(null, a[1, 0, 0])
+        assertEquals(null, a[-1, 0, 0])
+        assertEquals(1, a.chunkMap.size)
+
+        a[-1, 0, 0] = 'b'
+        assertEquals(2, a.size)
+        assertEquals('a', a[0, 0, 0])
+        assertEquals(null, a[1, 0, 0])
+        assertEquals('b', a[-1, 0, 0])
+        assertEquals(2, a.chunkMap.size)
+
+        a[1, 2, 3] = 'c'
+        assertEquals(3, a.size)
+        assertEquals(2, a.chunkMap.size)
+        a[-123, 456, -1] = 'd'
+        assertEquals(4, a.size)
+        assertEquals(3, a.chunkMap.size)
+        a[-123, 456, -1] = 'e'
+        assertEquals(4, a.size)
+        assertEquals(3, a.chunkMap.size)
+
+        assertEquals('a', a[0, 0, 0])
+        assertEquals(null, a[1, 0, 0])
+        assertEquals('b', a[-1, 0, 0])
+        assertEquals(null, a[-2, 0, 0])
+        assertEquals('c', a[1, 2, 3])
+        assertEquals('e', a[-123, 456, -1])
+
+        a[1, 2, 3] = null
+        assertEquals(null, a[1, 2, 3])
+        assertEquals(3, a.size)
+        assertEquals(3, a.chunkMap.size)
+    }
+}
