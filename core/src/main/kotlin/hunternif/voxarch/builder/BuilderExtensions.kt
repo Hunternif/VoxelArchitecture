b786114dfd69cb7d4268c1819cbfaf477a6586a0
Hunternif <hunternif@gmail.com>
2022-06-29 04:18:12 +0100

    Delete obsolete code: PositionTransformer, RoomConstrainedStorage, StructureUtil, fillXZ()

--- core/src/main/kotlin/hunternif/voxarch/builder/BuilderExtensions.kt
+++ core/src/main/kotlin/hunternif/voxarch/builder/BuilderExtensions.kt
@@ -3,11 +3,8 @@ package hunternif.voxarch.builder
 import hunternif.voxarch.plan.Room
 import hunternif.voxarch.storage.IBlockStorage
 import hunternif.voxarch.storage.TransformedBlockStorage
-import hunternif.voxarch.util.PositionTransformer
-import hunternif.voxarch.util.RoomConstrainedStorage
 import hunternif.voxarch.util.forEachXZ
 import hunternif.voxarch.vector.*
-import kotlin.math.roundToInt
 
 /**
  * Returns transformed storage, which accepts "local" coordinates
@@ -15,56 +12,6 @@ import kotlin.math.roundToInt
  */
 fun IBlockStorage.toLocal(trans: ILinearTransformation) =
     TransformedBlockStorage(this, trans)
-
-/**
- * Runs the function [buildAtXZ] at every (X,Z) point inside the room's walls.
- *
- * **Reference frame origin must be set to the low-XZ corner of the room!**
- *
- * When the room is rotated to a non-right angle, offsets are enabled:
- * * [rotationMargin] is distance inward from the walls to building site.
- * * [rotationOffset] is distance inward from the walls for constraint checking.
- * * [rotationStep] is increment for X & Z.
- * TODO: remove excessive margins & offsets.
- */
-fun IBlockStorage.fillXZ(
-    node: Room,
-    trans: TransformationStack,
-    rotationOffset: Double = 0.0,
-    rotationMargin: Double = 0.25,
-    rotationStep: Double = 0.5,
-    buildAtXZ: (x: Double, z: Double) -> Unit
-) {
-    val transformer = PositionTransformer(this)
-    transformer.angleY = trans.angleY
-    transformer.matrix = trans.matrix
-    val constraint = RoomConstrainedStorage(transformer, node)
-
-    val isRightAngle = transformer.angleY.roundToInt() % 90 == 0
-    // Step by 0.5 to prevent gaps when the node is rotated
-    // Optimization: if the angle is right, use 1.0 increments.
-    val step = if (isRightAngle) 1.0 else rotationStep
-
-    val isIntegerMove = transformer.transform(1.0, 1.0, 1.0).isInteger
-    // Extra margin on the edges is to prevent building outside walls.
-    // Optimization: if it maps precisely onto world blocks, use 0 margin.
-    val margin = if (isRightAngle) 0.0 else rotationMargin
-
-    val offset = if (isRightAngle && isIntegerMove) 0.0 else rotationOffset
-    constraint.setOffset(offset)
-
-    var x = margin
-    while (x <= node.width - margin) {
-        var z = margin
-        while (z <= node.length - margin) {
-            if (constraint.isWithinRoom(x, 0.0, z)) {
-                buildAtXZ(x, z)
-            }
-            z += step
-        }
-        x += step
-    }
-}
 
 /**
  * Runs the function [buildAt] at every (x, y, z) point inside the room's walls,
