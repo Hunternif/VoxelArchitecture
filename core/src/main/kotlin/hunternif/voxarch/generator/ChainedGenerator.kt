c277470e6ab6f80a3f779fc7237ff7f0c986beaf
Hunternif <hunternif@gmail.com>
2022-12-11 19:39:46 +0000

    Remove localStyle from ChainedGenerator (it's not used)

--- core/src/main/kotlin/hunternif/voxarch/generator/ChainedGenerator.kt
+++ core/src/main/kotlin/hunternif/voxarch/generator/ChainedGenerator.kt
@@ -1,19 +1,15 @@
 package hunternif.voxarch.generator
 
 import hunternif.voxarch.dom.builder.DomBuilder
-import hunternif.voxarch.dom.style.Stylesheet
-import hunternif.voxarch.dom.style.plus
 import hunternif.voxarch.plan.Node
 import hunternif.voxarch.util.CycleCounter
 import hunternif.voxarch.util.Recursive
-import kotlin.collections.LinkedHashSet
 
 /**
  * Allows running more generators on the output of this generator.
  */
 abstract class ChainedGenerator : Recursive(cycleCounter), IGenerator {
     val nextGens = LinkedHashSet<IGenerator>()
-    val localStyle = Stylesheet()
 
     /**
      * Add new DOM elements and run the next generators
@@ -26,12 +22,7 @@ abstract class ChainedGenerator : Recursive(cycleCounter), IGenerator {
 
     override fun generate(parent: DomBuilder, parentNode: Node) = guard {
         parent.apply {
-            // Add custom stylesheet
-            val originalStyle = stylesheet
-            stylesheet = originalStyle + localStyle
             generateChained(this, parentNode) {
-                // Restore original stylesheet
-                stylesheet = originalStyle
                 nextGens.forEach { it.generate(this, parentNode) }
             }
         }
