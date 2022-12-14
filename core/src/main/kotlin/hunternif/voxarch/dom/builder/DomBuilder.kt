16c0936853e4f250dcb93e640cf1a91b0c00857d
Hunternif <hunternif@gmail.com>
2022-12-14 00:15:30 +0000

    Move DomBuildContext to a separate file

--- core/src/main/kotlin/hunternif/voxarch/dom/builder/DomBuilder.kt
+++ core/src/main/kotlin/hunternif/voxarch/dom/builder/DomBuilder.kt
@@ -1,8 +1,6 @@
 package hunternif.voxarch.dom.builder
 
 import hunternif.voxarch.dom.CastleDsl
-import hunternif.voxarch.dom.style.Stylesheet
-import hunternif.voxarch.plan.Node
 import hunternif.voxarch.util.CycleCounter
 import hunternif.voxarch.util.Recursive
 
@@ -73,44 +71,6 @@ open class DomBuilder : Recursive(cycleCounter) {
     companion object {
         val cycleCounter = CycleCounter(20)
     }
-}
-
-/** Passed into children during building DOM tree. */
-//TODO: maybe re-use StyledElement for this
-data class DomBuildContext(
-    /** Immediate parent DOM element. */
-    val parent: DomBuilder,
-
-    /** Immediate parent Node that is already built. */
-    val parentNode: Node,
-
-    /** this stylesheet will apply to all elements in this DOM. */
-    val stylesheet: Stylesheet,
-
-    /**
-     * Each child element will receive a seed value that's derived
-     * from this root seed value by a deterministic arithmetic.
-     */
-    val seed: Long,
-
-    /** Style classes inherited from all parent DOM elements. */
-    val inheritedStyleClass: MutableSet<String> = linkedSetOf(),
-) {
-    fun inherit(ctx: DomBuildContext): DomBuildContext {
-        inheritedStyleClass.addAll(ctx.inheritedStyleClass)
-        return this
-    }
-    fun inherit(styleClasses: Iterable<String>): DomBuildContext {
-        inheritedStyleClass.addAll(styleClasses)
-        return this
-    }
-    fun copy(
-        parent: DomBuilder = this.parent,
-        parentNode: Node = this.parentNode,
-        stylesheet: Stylesheet = this.stylesheet,
-        seed: Long = this.seed,
-    ) = DomBuildContext(parent, parentNode, stylesheet, seed)
-        .inherit(inheritedStyleClass)
 }
 
 enum class Visibility {
