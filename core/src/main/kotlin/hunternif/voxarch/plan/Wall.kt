06811f0eec1004750eb8d44851cb2dce08adafe9
Hunternif <hunternif@gmail.com>
2022-12-20 02:14:08 +0000

    Wall position is stored in polar coordinates (angle + length + height)

--- core/src/main/kotlin/hunternif/voxarch/plan/Wall.kt
+++ core/src/main/kotlin/hunternif/voxarch/plan/Wall.kt
@@ -1,8 +1,9 @@
 package hunternif.voxarch.plan
 
+import hunternif.voxarch.util.MathUtil
+import hunternif.voxarch.util.rotateY
 import hunternif.voxarch.vector.Vec2
 import hunternif.voxarch.vector.Vec3
-import kotlin.math.atan2
 
 /**
  * ```
@@ -15,7 +16,10 @@ import kotlin.math.atan2
  *  V
  *  Z
  * ```
- * [rotationY] is read-only and is determined by `origin` and `end`.
+ *
+ * Internally the state is represented via rotation + length + height.
+ * The rest is computed!
+ *
  * @param start relative to _parent's origin_, Y = floor level, will be copied.
  * @param end relative to _parent's origin_, Y = wall height, will be copied.
  */
@@ -25,49 +29,40 @@ open class Wall(
     /** If true, the wall will not be generated, only used as boundary for calculations. */
     var transparent: Boolean = false
 ) : Node(start) {
-    /** Start point on the ground, vs parent origin. */
+
+    final override var rotationY: Double
+        get() = super.rotationY
+        set(value) {
+            super.rotationY = value
+            direction.set(Vec3.UNIT_X.rotateY(rotationY))
+        }
+
+    /** Helper vector that points from origin in the direction of rotation. */
+    protected val direction: Vec3 = Vec3(0, 0, 0)
+
+    init {
+        length = origin.toXZ().distanceTo(end.toXZ())
+        height = end.y - origin.y
+        rotationY = MathUtil.atan2Deg(-end.z + origin.z, end.x - origin.x)
+    }
+
+    /** Start point on the ground, vs parent origin. Read-only. */
     val bottomStart: Vec3 get() = origin
-    /** End point on the ground, vs parent origin. */
-    val bottomEnd: Vec3 get() = Vec3(end.x, origin.y, end.z)
 
-    /** Top far point relative to local origin */
-    val innerEnd: Vec3 = end.subtract(start)
-    /** Top far point relative to parent origin.
-     * It's read-only, so that the entire wall can be moved by moving origin.
-     * To move the end, change [innerEnd]. */
-    val end: Vec3 get() = origin.add(innerEnd)
+    /** End point on the ground, vs parent origin. Read-only. */
+    val bottomEnd: Vec3 get() = origin + direction * length
 
-    /** Read-only, determined from start and end. */
-    override var rotationY: Double
-        get() = atan2(-innerEnd.z, innerEnd.x) * 180 / Math.PI
-        set(_) {}
+    /** Top far point relative to local origin. Read-only. */
+    val innerEnd: Vec3 get() = direction * length + Vec3.UNIT_Y * height
 
-    /** XZ of start point, vs parent origin. */
+    /** Top far point relative to parent origin. Read-only. */
+    val end: Vec3 get() = origin + innerEnd
+
+    /** XZ of start point, vs parent origin. Read-only. */
     val p1: Vec2 get() = Vec2.fromXZ(origin)
-    /** XZ of end point, vs parent origin. */
+
+    /** XZ of end point, vs parent origin. Read-only. */
     val p2: Vec2 get() = Vec2.fromXZ(end)
-
-    override var size: Vec3 = Vec3(0, 0, 0)
-        get() = field.set(length, height, 0.0)
-        set(value) {
-            length = value.x
-            height = value.y
-        }
-    override var length: Double
-        get() = bottomEnd.distanceTo(bottomStart)
-        set(value) {
-            if (length == 0.0) {
-                innerEnd.x = value
-            } else {
-                val ratio = value / length
-                innerEnd.x *= ratio
-                innerEnd.z *= ratio
-            }
-        }
-    override var height: Double
-        get() = innerEnd.y
-        set(value) { innerEnd.y = value }
-
 
     /** Legacy constructor */
     constructor(room: Room, p1: Vec2, p2: Vec2, transparent: Boolean):
@@ -78,8 +73,9 @@ open class Wall(
     ) {
         this.parent = room
     }
-    /** Legacy constructor */
-    constructor(room: Room, p1: Vec2, p2: Vec2): this(room, p1, p2, false)
 
-    constructor(): this(Vec3.ZERO, Vec3.ZERO)
+    /** Legacy constructor */
+    constructor(room: Room, p1: Vec2, p2: Vec2) : this(room, p1, p2, false)
+
+    constructor() : this(Vec3.ZERO, Vec3.ZERO)
 }
