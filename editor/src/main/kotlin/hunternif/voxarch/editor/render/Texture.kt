3c6a27b4d6435502a0c42f19c4dfac7fc89b9dee
Hunternif <hunternif@gmail.com>
2023-01-20 01:52:25 +0000

    Editor: refactor Texture

--- editor/src/main/kotlin/hunternif/voxarch/editor/render/Texture.kt
+++ editor/src/main/kotlin/hunternif/voxarch/editor/render/Texture.kt
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.GL32.*
 import org.lwjgl.stb.STBImage.stbi_image_free
 import org.lwjgl.stb.STBImage.stbi_load
 import org.lwjgl.system.MemoryStack
+import java.nio.ByteBuffer
 
 class Texture(val filepath: String) {
     /** This texture's ID for OpenGL */
@@ -17,10 +18,13 @@ class Texture(val filepath: String) {
         private set
     var height: Int = 0
         private set
+    var channels: Int = 0
+        private set
 
     fun generate(width: Int, height: Int) {
         this.width = width
         this.height = height
+        this.channels = 3
         // Generate texture on GPU
         texID = glGenTextures()
         glBindTexture(GL_TEXTURE_2D, texID)
@@ -41,7 +45,35 @@ class Texture(val filepath: String) {
         )
     }
 
-    fun load() = MemoryStack.stackPush().use { stack ->
+    /** Loads the texture from file and uploads it to the GPU. */
+    fun load() {
+        val bytes = loadData()
+        uploadToGPU(bytes)
+        stbi_image_free(bytes)
+    }
+
+    /**
+     * Loads the texture bytes from file, without uploading it to the GPU.
+     * This also sets parameters [width], [height], [channels].
+     */
+    fun loadData(): ByteBuffer = MemoryStack.stackPush().use { stack ->
+        val width = stack.mallocInt(1)
+        val height = stack.mallocInt(1)
+        val channels = stack.mallocInt(1)
+        val bytes = stbi_load(filepath, width, height, channels, 0)
+            ?: throw RuntimeException("Error: (Texture) Could not load image '$filepath'")
+        this.width = width[0]
+        this.height = height[0]
+        this.channels = channels[0]
+        if (this.channels != 3 && this.channels != 4) {
+            throw RuntimeException("Error: (Texture) Unknown number of channels '${this.channels}'")
+        }
+        return bytes
+    }
+
+    /** Uploads to GPU.
+     * Assuming that [width], [height], [channels] have been set. */
+    fun uploadToGPU(bytes: ByteBuffer) {
         // Generate texture on GPU
         texID = glGenTextures()
         glBindTexture(GL_TEXTURE_2D, texID)
@@ -54,34 +86,22 @@ class Texture(val filepath: String) {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
         // When stretching an image, pixelate
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        val width = stack.mallocInt(1)
-        val height = stack.mallocInt(1)
-        val channels = stack.mallocInt(1)
-        val image = stbi_load(filepath, width, height, channels, 0)
-        if (image != null) {
-            this.width = width[0]
-            this.height = height[0]
-            if (channels[0] == 3) {
-                glTexImage2D(
-                    GL_TEXTURE_2D, 0, GL_RGB,
-                    width[0], height[0],
-                    0, GL_RGB, GL_UNSIGNED_BYTE, image
-                )
-            } else if (channels[0] == 4) {
-                glTexImage2D(
-                    GL_TEXTURE_2D, 0, GL_RGBA,
-                    width[0], height[0],
-                    0, GL_RGBA, GL_UNSIGNED_BYTE, image
-                )
-            } else {
-                throw RuntimeException("Error: (Texture) Unknown number of channels '${channels[0]}'")
-            }
-        } else {
-            throw RuntimeException("Error: (Texture) Could not load image '$filepath'")
+        if (channels == 3) {
+            glTexImage2D(
+                GL_TEXTURE_2D, 0, GL_RGB,
+                width, height,
+                0, GL_RGB, GL_UNSIGNED_BYTE, bytes
+            )
+        } else if (channels == 4) {
+            glTexImage2D(
+                GL_TEXTURE_2D, 0, GL_RGBA,
+                width, height,
+                0, GL_RGBA, GL_UNSIGNED_BYTE, bytes
+            )
         }
-        stbi_image_free(image)
         isLoaded = true
     }
+
 
     /** Set OpenGL to sample from this texture. */
     fun bind() {
