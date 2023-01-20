9904c30e623b0a2272d84e7b8bdbd8cd0986e9c7
Hunternif <hunternif@gmail.com>
2023-01-20 00:47:02 +0000

    Editor: update texture atlas to actually copy the texture and use correct UVs

--- editor/src/main/kotlin/hunternif/voxarch/editor/render/TextureAtlas.kt
+++ editor/src/main/kotlin/hunternif/voxarch/editor/render/TextureAtlas.kt
@@ -2,14 +2,16 @@ package hunternif.voxarch.editor.render
 
 import org.joml.Vector2f
 import org.joml.Vector2i
+import org.lwjgl.opengl.GL32.*
 import kotlin.math.max
 
 /**
  * Stitches textures together row by row.
  */
-class TextureAtlas(
-    val sheet: Texture,
-) {
+class TextureAtlas(val width: Int, val height: Int) {
+    val sheet = Texture("sheet")
+    private var mainFboID: Int = 0
+
     private val entries = linkedSetOf<AtlasEntry>()
 
     /** Points to the nearest free space, to its top-left corner. */
@@ -18,37 +20,101 @@ class TextureAtlas(
     /** Max height of textures in the current row. */
     private var rowHeight = 0
 
+    fun init() {
+        sheet.generate(width, height)
+        mainFboID = glGenFramebuffers()
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, mainFboID)
+        glFramebufferTexture2D(
+            GL_DRAW_FRAMEBUFFER,
+            GL_COLOR_ATTACHMENT0,
+            GL_TEXTURE_2D,
+            sheet.texID,
+            0
+        )
+    }
+
     /** Adds a new texture to the atlas, if it fits. */
     fun add(texture: Texture): AtlasEntry {
-        if (cursor.x + texture.width > sheet.width) {
+        if (!texture.isLoaded) texture.load()
+        updateCursor(texture)
+
+        val entry = AtlasEntry(this, texture, cursor)
+        entries.add(entry)
+
+        copyTexture(entry)
+
+        cursor.x += texture.width
+        rowHeight = max(rowHeight, texture.height)
+        return entry
+    }
+
+    /** Shifts cursor to accommodate a given new texture. */
+    private fun updateCursor(newTexture: Texture) {
+        if (cursor.x + newTexture.width > width) {
             // start a new row
-            if (cursor.y + rowHeight > sheet.height) {
+            if (cursor.y + rowHeight > height) {
                 throw ArrayIndexOutOfBoundsException(
-                    "Texture ${texture.filepath} doesn't fit into atlas."
+                    "Texture ${newTexture.filepath} doesn't fit into atlas."
                 )
             }
             cursor.y += rowHeight
             cursor.x = 0
             rowHeight = 0
         }
-        if (cursor.y + texture.height > sheet.height) {
+        if (cursor.y + newTexture.height > height) {
             throw ArrayIndexOutOfBoundsException(
-                "Texture ${texture.filepath} doesn't fit into atlas."
+                "Texture ${newTexture.filepath} doesn't fit into atlas."
             )
         }
-        val entry = AtlasEntry(
-            texture, Vector2f(cursor),
-            Vector2f(cursor).add(texture.width.toFloat(), texture.height.toFloat())
+    }
+
+    /** Renders the entry's texture onto the main sheet texture at [cursor]. */
+    private fun copyTexture(entry: AtlasEntry) {
+        val texWidth = entry.texture.width
+        val texHeight = entry.texture.height
+
+        val texFboID = glGenFramebuffers()
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, mainFboID)
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, texFboID)
+        glFramebufferTexture2D(
+            GL_READ_FRAMEBUFFER,
+            GL_COLOR_ATTACHMENT0,
+            GL_TEXTURE_2D,
+            entry.texture.texID,
+            0
         )
-        entries.add(entry)
-        cursor.x += texture.width
-        rowHeight = max(rowHeight, texture.height)
-        return entry
+        glBlitFramebuffer(
+            0, 0, texWidth, texHeight,
+            entry.start.x, entry.start.y,
+            entry.end.x, entry.end.y,
+            GL_COLOR_BUFFER_BIT, GL_NEAREST,
+        )
+        glBindFramebuffer(GL_FRAMEBUFFER, 0)
     }
 }
 
-data class AtlasEntry(
+class AtlasEntry(
+    /** The combined atlas texture. */
+    atlas: TextureAtlas,
+    /** The original texture file. */
     val texture: Texture,
-    val uvStart: Vector2f,
-    val uvEnd: Vector2f,
-)
+    start: Vector2i,
+) {
+    /** Start corner in absolute texture coordinates. */
+    val start: Vector2i = Vector2i(start)
+
+    /** End corner in absolute texture coordinates. */
+    val end: Vector2i = Vector2i(start).add(texture.width, texture.height)
+
+    /** UV of the start corner on the atlas texture. */
+    val uvStart: Vector2f = Vector2f(
+        start.x.toFloat() / atlas.sheet.width.toFloat(),
+        start.y.toFloat() / atlas.sheet.height.toFloat(),
+    )
+
+    /** UV of the end corner on the atlas texture. */
+    val uvEnd: Vector2f = Vector2f(
+        end.x.toFloat() / atlas.sheet.width.toFloat(),
+        end.y.toFloat() / atlas.sheet.height.toFloat(),
+    )
+}
--- editor/src/test/kotlin/hunternif/voxarch/editor/render/TextureAtlasTest.kt
+++ editor/src/test/kotlin/hunternif/voxarch/editor/render/TextureAtlasTest.kt
@@ -2,72 +2,82 @@ package hunternif.voxarch.editor.render
 
 import hunternif.voxarch.editor.BaseAppTest
 import org.joml.Vector2f
+import org.joml.Vector2i
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
 class TextureAtlasTest : BaseAppTest() {
-    private val sheet = Texture("sheet")
     private val tex1x1 = Texture("1x1")
     private val tex2x1 = Texture("2x1")
     private val tex1x2 = Texture("1x2")
+    private val tex2x2 = Texture("2x2")
 
     @Before
     fun setup() {
         tex1x1.generate(1, 1)
         tex2x1.generate(2, 1)
         tex1x2.generate(1, 2)
+        tex2x2.generate(2, 2)
+    }
+
+    @Test
+    fun `calculate UVs`() {
+        val atlas = TextureAtlas(4, 4)
+        atlas.init()
+        atlas.add(tex1x1)
+        val e2 = atlas.add(tex2x2)
+        assertEquals(Vector2i(1, 0), e2.start)
+        assertEquals(Vector2i(3, 2), e2.end)
+        assertEquals(Vector2f(0.25f, 0f), e2.uvStart)
+        assertEquals(Vector2f(0.75f, 0.5f), e2.uvEnd)
     }
 
     @Test
     fun `uniform size`() {
-        sheet.generate(2, 2)
-        val atlas = TextureAtlas(sheet)
+        val atlas = TextureAtlas(2, 2)
         val e1 = atlas.add(tex1x1)
         val e2 = atlas.add(tex1x1)
         val e3 = atlas.add(tex1x1)
         val e4 = atlas.add(tex1x1)
-        assertEquals(Vector2f(0f, 0f), e1.uvStart)
-        assertEquals(Vector2f(1f, 1f), e1.uvEnd)
-        assertEquals(Vector2f(1f, 0f), e2.uvStart)
-        assertEquals(Vector2f(2f, 1f), e2.uvEnd)
-        assertEquals(Vector2f(0f, 1f), e3.uvStart)
-        assertEquals(Vector2f(1f, 2f), e3.uvEnd)
-        assertEquals(Vector2f(1f, 1f), e4.uvStart)
-        assertEquals(Vector2f(2f, 2f), e4.uvEnd)
+        assertEquals(Vector2i(0, 0), e1.start)
+        assertEquals(Vector2i(1, 1), e1.end)
+        assertEquals(Vector2i(1, 0), e2.start)
+        assertEquals(Vector2i(2, 1), e2.end)
+        assertEquals(Vector2i(0, 1), e3.start)
+        assertEquals(Vector2i(1, 2), e3.end)
+        assertEquals(Vector2i(1, 1), e4.start)
+        assertEquals(Vector2i(2, 2), e4.end)
     }
 
     @Test
     fun `uneven horizontal size`() {
-        sheet.generate(2, 2)
-        val atlas = TextureAtlas(sheet)
+        val atlas = TextureAtlas(2, 2)
         val e1 = atlas.add(tex1x1)
         val e2 = atlas.add(tex2x1)
-        assertEquals(Vector2f(0f, 0f), e1.uvStart)
-        assertEquals(Vector2f(1f, 1f), e1.uvEnd)
-        assertEquals(Vector2f(0f, 1f), e2.uvStart)
-        assertEquals(Vector2f(2f, 2f), e2.uvEnd)
+        assertEquals(Vector2i(0, 0), e1.start)
+        assertEquals(Vector2i(1, 1), e1.end)
+        assertEquals(Vector2i(0, 1), e2.start)
+        assertEquals(Vector2i(2, 2), e2.end)
     }
 
     @Test
     fun `uneven row height`() {
-        sheet.generate(2, 3)
-        val atlas = TextureAtlas(sheet)
+        val atlas = TextureAtlas(2, 3)
         val e1 = atlas.add(tex1x1)
         val e2 = atlas.add(tex1x2)
         val e3 = atlas.add(tex1x1)
-        assertEquals(Vector2f(0f, 0f), e1.uvStart)
-        assertEquals(Vector2f(1f, 1f), e1.uvEnd)
-        assertEquals(Vector2f(1f, 0f), e2.uvStart)
-        assertEquals(Vector2f(2f, 2f), e2.uvEnd)
-        assertEquals(Vector2f(0f, 2f), e3.uvStart)
-        assertEquals(Vector2f(1f, 3f), e3.uvEnd)
+        assertEquals(Vector2i(0, 0), e1.start)
+        assertEquals(Vector2i(1, 1), e1.end)
+        assertEquals(Vector2i(1, 0), e2.start)
+        assertEquals(Vector2i(2, 2), e2.end)
+        assertEquals(Vector2i(0, 2), e3.start)
+        assertEquals(Vector2i(1, 3), e3.end)
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException::class)
     fun `throw if texture doesn't fit`() {
-        sheet.generate(2, 2)
-        val atlas = TextureAtlas(sheet)
+        val atlas = TextureAtlas(2, 2)
         atlas.add(tex1x1)
         atlas.add(tex1x1)
         atlas.add(tex1x2)
