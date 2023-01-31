b2ba098b119bfc812d732deb2a15e15d00fc4800
Hunternif <hunternif@gmail.com>
2023-01-31 00:03:15 +0000

    Use layout in DomTranslateBuilder to offset child nodes

--- core/src/main/kotlin/hunternif/voxarch/dom/builder/DomTranslateBuilder.kt
+++ core/src/main/kotlin/hunternif/voxarch/dom/builder/DomTranslateBuilder.kt
@@ -1,21 +1,17 @@
 package hunternif.voxarch.dom.builder
 
 import hunternif.voxarch.dom.style.StyledElement
-import hunternif.voxarch.dom.style.property.*
-import hunternif.voxarch.dom.style.selectInherit
+import hunternif.voxarch.dom.style.StyledNode
 import hunternif.voxarch.vector.Vec3
 
 class DomTranslateBuilder(
     private val offset: Vec3,
 ) : DomBuilder() {
-    override fun prepareForLayout(ctx: DomBuildContext): StyledElement<*> {
-        children.forEach {
-            ctx.stylesheet.add {
-                style(selectInherit(this@DomTranslateBuilder).instance(it)) {
-                    position { origin, _ -> origin + offset }
-                }
-            }
+
+    override fun layout(children: List<StyledElement<*>>): List<StyledElement<*>> {
+        children.filterIsInstance<StyledNode<*>>().forEach {
+            it.node.origin.addLocal(offset)
         }
-        return super.prepareForLayout(ctx)
+        return children
     }
 }
