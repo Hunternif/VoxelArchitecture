d19ebdb9f6bd54ed283cf351a1b1443c4782cd22
Hunternif <hunternif@gmail.com>
2023-02-02 22:20:47 +0000

    Simplify DomSubdivideY

--- core/src/main/kotlin/hunternif/voxarch/dom/builder/DomSubdivideY.kt
+++ core/src/main/kotlin/hunternif/voxarch/dom/builder/DomSubdivideY.kt
@@ -4,6 +4,7 @@ import hunternif.voxarch.dom.style.StyledElement
 import hunternif.voxarch.dom.style.StyledNode
 import hunternif.voxarch.dom.style.pct
 import hunternif.voxarch.dom.style.property.PropHeight
+import hunternif.voxarch.plan.Node
 import kotlin.math.min
 import kotlin.math.round
 
@@ -13,62 +14,54 @@ import kotlin.math.round
  */
 class DomSubdivideY : DomBuilder() {
     override fun layout(children: List<StyledElement<*>>): List<StyledElement<*>> {
-        val items = children
-            .filterIsInstance<StyledNode<*>>()
-            .associateWith {
-                val props = it.ctx.stylesheet.getProperties(it)
-                props[PropHeight]?.value ?: 100.pct
-            }
-        if (items.isEmpty()) return children
+        if (children.isEmpty()) return children
 
-        val parentNode = items.keys.first().parentNode
+        val allNodes = ArrayList<Node>(children.size)
+        val solidNodes = ArrayList<Node>(children.size)
+        val pctNodes = ArrayList<Node>(children.size)
+        children.forEach {
+            if (it is StyledNode<*>) {
+                allNodes.add(it.node)
+                val props = it.ctx.stylesheet.getProperties(it)
+                val height = props[PropHeight]?.value ?: 100.pct
+                if (height.isPct) pctNodes.add(it.node)
+                else solidNodes.add(it.node)
+            }
+        }
+
+        val parentNode = children.first().parentNode
         // + 1 to get "natural" size
         val total = parentNode.height + 1
 
         // Get "natural" sizes pre-layout
-        var solidTotal = 0.0 // total "natural" size of solid nodes
-        var pctTotal = 0.0   // total "natural" size of % nodes
-        var solidCount = 0   // number of solid nodes
-        var pctCount = 0     // number of % nodes
-        items.forEach { (k, v) ->
-            if (v.isPct) {
-                pctTotal += k.node.height + 1
-                pctCount++
-            } else {
-                solidTotal += k.node.height + 1
-                solidCount++
-            }
-        }
+        val solidTotal = solidNodes.fold(0.0) { o, node -> o + node.height + 1}
+        val pctTotal = pctNodes.fold(0.0) { o, node -> o + node.height + 1}
 
-        if (pctTotal - pctCount > 0.0) {
+        if (pctTotal - pctNodes.size > 0.0) {
             // Adjust percentage sizes based on remaining "centric" total size.
-            // (- pctCount because each node will occupy 1 extra block)
             var remaining = total - solidTotal // "natural size"
-            val ratio = (remaining - pctCount) / (pctTotal - pctCount)
-            items.forEach { (k, v) ->
-                if (v.isPct) {
-                    // min value -1 represents a node that's absolutely invisibly flat
-                    if (remaining <= -1) k.node.height = -1.0
-                    else {
-                        val newSize = min(remaining - 1, round(k.node.height * ratio))
-                        k.node.height = newSize
-                        remaining -= newSize + 1
-                    }
+            val ratio = (remaining - pctNodes.size) / (pctTotal - pctNodes.size)
+            pctNodes.forEach {
+                // min value -1 represents a node that's absolutely invisibly flat
+                if (remaining <= -1) it.height = -1.0
+                else {
+                    val newSize = min(remaining - 1, round(it.height * ratio))
+                    it.height = newSize
+                    remaining -= newSize + 1
                 }
             }
             if (remaining > 0) {
                 // must be a tiny fraction left, add it to the first % element:
                 // TODO: create a first / last / symmetry bias in subdivide layout
-                val firstPctEntry = items.entries.first { (k, v) -> v.isPct}
-                firstPctEntry.key.node.height += remaining
+                pctNodes.first().height += remaining
             }
         }
 
         // Lay out positions going bottom up:
         var y = 0.0
-        items.keys.forEach {
-            it.node.origin.y = y
-            y += it.node.height + 1
+        allNodes.forEach {
+            it.origin.y = y
+            y += it.height + 1
         }
 
         return children
