eb9195a9b975f91360ca59a9b25977909e1f044e
Hunternif <hunternif@gmail.com>
2023-02-05 13:20:51 +0000

    Add comments to Direction3D

--- core/src/main/kotlin/hunternif/voxarch/util/Direction3D.kt
+++ core/src/main/kotlin/hunternif/voxarch/util/Direction3D.kt
@@ -3,12 +3,27 @@ package hunternif.voxarch.util
 import hunternif.voxarch.vector.IntVec3
 import hunternif.voxarch.util.Direction3D.*
 
+/**
+ * ```
+ * Y
+ *  +----> X (East)
+ *  |
+ *  V
+ *  Z
+ * ```
+ */
 enum class Direction3D(val vec: IntVec3) {
+    /** Positive Y */
     UP(IntVec3(0, 1, 0)),
+    /** Negative Y */
     DOWN(IntVec3(0, -1, 0)),
+    /** Positive X */
     EAST(IntVec3(1, 0, 0)),
-    SOUTH(IntVec3(0, 0, 1)),
+    /** Negative X */
     WEST(IntVec3(-1, 0, 0)),
+    /** Positive Z */
+    SOUTH(IntVec3(0, 0, 1)),
+    /** Negative Z */
     NORTH(IntVec3(0, 0, -1))
 }
 
