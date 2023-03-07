9c617033584f7a353a42ac89955c05bca5d5d3c2
Hunternif <hunternif@gmail.com>
2023-03-07 02:29:24 +0000

    Editor: Grammar: add tests for invalid input

--- editor/src/test/kotlin/hunternif/voxarch/editor/antlr/StyleGrammarParserTest.kt
+++ editor/src/test/kotlin/hunternif/voxarch/editor/antlr/StyleGrammarParserTest.kt
@@ -1,7 +1,9 @@
 package hunternif.voxarch.editor.antlr
 
+import org.antlr.v4.runtime.BailErrorStrategy
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.misc.ParseCancellationException
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -16,9 +18,15 @@ class StyleGrammarParserTest {
         )
     }
 
+    @Test(expected = ParseCancellationException::class)
+    fun `parse invalid empty selector`() {
+        parseAndFormat("{ prop: value }")
+    }
+
     private fun parseAndFormat(input: String): String {
         val lexer = StyleGrammarLexer(CharStreams.fromString(input))
         val parser = StyleGrammarParser(CommonTokenStream(lexer))
+        parser.errorHandler = BailErrorStrategy()
         return parser.stylesheet().toParseTree().multiLineString()
     }
 }
