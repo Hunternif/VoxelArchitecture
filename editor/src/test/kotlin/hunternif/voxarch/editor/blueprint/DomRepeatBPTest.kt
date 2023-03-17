77ec26ff75685d918041f4c254c792eccba2b8d3
Hunternif <hunternif@gmail.com>
2023-03-17 00:13:24 +0000

    Add project-based unit test for DomRepeat

---
+++ editor/src/test/kotlin/hunternif/voxarch/editor/blueprint/DomRepeatBPTest.kt
@@ -0,0 +1,22 @@
+package hunternif.voxarch.editor.blueprint
+
+import hunternif.voxarch.editor.BaseAppTest
+import hunternif.voxarch.editor.actions.generateNodes
+import hunternif.voxarch.editor.actions.openProjectFile
+import hunternif.voxarch.editor.file.serializeToXmlStr
+import hunternif.voxarch.editor.util.resourcePath
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DomRepeatBPTest : BaseAppTest() {
+    // Opens a test project file, executes the blueprint, and verifies node tree.
+    @Test
+    fun `run blueprint with DomRepeat`() {
+        val path = resourcePath("project/repeat_test.voxarch")
+        app.openProjectFile(path)
+        val refNodeTree = serializeToXmlStr(app.state.rootNode.node, true)
+        app.generateNodes()
+        val generatedNodeTree = serializeToXmlStr(app.state.rootNode.node, true)
+        assertEquals(refNodeTree, generatedNodeTree)
+    }
+}
