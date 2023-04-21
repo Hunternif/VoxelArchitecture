98dfa1b917af8d428f6dbe3f25278c2132dc4281
Hunternif <hunternif@gmail.com>
2023-04-21 23:10:43 +0100

    Fix bug when reading large vox files from project ZIP file (wrong byte order)

--- core/src/main/kotlin/hunternif/voxarch/magicavoxel/VoxToArray3DReader.kt
+++ core/src/main/kotlin/hunternif/voxarch/magicavoxel/VoxToArray3DReader.kt
@@ -2,6 +2,7 @@ package hunternif.voxarch.magicavoxel
 
 import com.scs.voxlib.VoxReader
 import hunternif.voxarch.vector.Array3D
+import java.io.ByteArrayInputStream
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -9,7 +10,7 @@ fun readVoxFile(
     path: Path,
     useModelOffset: Boolean = true,
 ): VoxFileStorage {
-    val voxFile = VoxReader(Files.newInputStream(path)).use { it.read() }
+    val voxFile = VoxReader(byteStreamFromFile(path)).use { it.read() }
     return VoxFileStorage.fromFile(voxFile, useModelOffset)
 }
 
@@ -17,9 +18,14 @@ inline fun <reified C: Any> readVoxFile(
     path: Path,
     colorMap: (VoxColor?) -> C
 ): Array3D<C> {
-    val voxFile = VoxReader(Files.newInputStream(path)).use { it.read() }
+    val voxFile = VoxReader(byteStreamFromFile(path)).use { it.read() }
     val storage = VoxFileStorage.fromFile(voxFile)
     return Array3D(storage.width, storage.height, storage.depth) { x, y, z ->
         colorMap(storage[x, y, z])
     }
 }
+
+fun byteStreamFromFile(path: Path) =
+    ByteArrayInputStream(
+        Files.newInputStream(path).use { it.readBytes() }
+    )
