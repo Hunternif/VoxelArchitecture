95df3f38a2553af01ca0aac4e8d6b347d7bfe606
Hunternif <hunternif@gmail.com>
2023-05-24 23:44:46 +0100

    Editor: set "built voxels" origin roughly in the middle

--- editor/src/main/kotlin/hunternif/voxarch/editor/actions/scene/BuildVoxels.kt
+++ editor/src/main/kotlin/hunternif/voxarch/editor/actions/scene/BuildVoxels.kt
@@ -2,18 +2,20 @@ package hunternif.voxarch.editor.actions.scene
 
 import hunternif.voxarch.builder.IBuildListener
 import hunternif.voxarch.builder.RootBuilder
+import hunternif.voxarch.builder.toLocal
 import hunternif.voxarch.editor.EditorApp
 import hunternif.voxarch.editor.EditorAppImpl
 import hunternif.voxarch.editor.actions.history.HistoryAction
 import hunternif.voxarch.editor.actions.logInfo
 import hunternif.voxarch.editor.actions.redrawVoxels
 import hunternif.voxarch.editor.gui.FontAwesomeIcons
-import hunternif.voxarch.editor.scenegraph.DetachedObject
-import hunternif.voxarch.editor.scenegraph.SceneVoxelGroup
-import hunternif.voxarch.editor.scenegraph.detached
+import hunternif.voxarch.editor.scenegraph.*
 import hunternif.voxarch.plan.Node
 import hunternif.voxarch.storage.BlockStorageDelegate
 import hunternif.voxarch.storage.ChunkedStorage3D
+import hunternif.voxarch.util.round
+import hunternif.voxarch.vector.LinearTransformation
+import hunternif.voxarch.vector.Vec3
 
 class BuildVoxels : HistoryAction(
     "Build voxels",
@@ -33,9 +35,17 @@ class BuildVoxels : HistoryAction(
             val builder = RootBuilder()
             app.state.run {
                 if (verboseBuild) builder.addListener(VerboseLogger(app))
-                builder.build(rootNode.node, world, buildContext)
+
+                // move the future voxel group's origin:
+                val newOrigin = findAvgOrigin(rootNode).round()
+                val transform = LinearTransformation().translate(-newOrigin)
+                val offsetWorld = world.toLocal(transform)
+
+                builder.build(rootNode.node, offsetWorld, buildContext)
                 val builtVoxels = registry.newVoxelGroup(
                     "Built voxels", world, renderMode, true)
+                builtVoxels.origin.set(newOrigin)
+
                 voxelRoot.addChild(builtVoxels)
                 newGenerated = listOf(builtVoxels.detached())
             }
@@ -60,6 +70,24 @@ class BuildVoxels : HistoryAction(
     private fun EditorAppImpl.clearGeneratedVoxels() = state.run {
         generatedVoxels.toList().forEach { it.remove() }
         generatedVoxels.clear()
+    }
+
+    /**
+     * Determines where to place the new "built voxels" group, so that
+     * its origin is roughly in the middle of all nodes.
+     */
+    private fun findAvgOrigin(root: SceneNode): Vec3 {
+        // Only count nodes that will produce voxels:
+        val nodesWithBlueprints = root.children.filterIsInstance<SceneNode>()
+            .filter { it.blueprints.isNotEmpty() }
+        if (nodesWithBlueprints.isEmpty()) return root.node.origin
+
+        // root node is usually at (0, 0, 0), but its children are offset
+        val avgChildPos = nodesWithBlueprints.fold(Vec3(0, 0, 0)) { vec, child ->
+            vec + child.node.origin
+        } / nodesWithBlueprints.size
+
+        return avgChildPos + root.node.origin
     }
 
     companion object {
